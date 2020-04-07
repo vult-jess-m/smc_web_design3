e3950186d20196d8022ecfc47e78fcbd69a1b373
vult-jess-m <61213589+vult-jess-m@users.noreply.github.com>
2020-04-07 15:10:32 -0700

    Errors
    Fixed carousel and changed images. added height in css so images in carousel are the same.

--- Homework/Midterm Dry Run/script.js
+++ Homework/Midterm Dry Run/script.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
 
-    $('bugCarousel').carousel({
+    $('#bugCarousel').carousel({
         interval: 580
     });
 
+    $('#carouselControl').click(function(){
+        
+        if( $('#carouselControl').hasClass('paused') ){
+            $('#bugCarousel').carousel('cycle');
+            $('#carouselControl').text('Pause');
+        }
+        else {
+            $('#myCarousel').carousel('pause');
+            $('#carouselControl').text('Play');
+        }
+
+        $('#carouselControl').toggleClass('paused');
+    });
 });

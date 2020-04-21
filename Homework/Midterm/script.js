3ae0b446507085169314debad7ac312aecbb72b5
vult-jess-m <61213589+vult-jess-m@users.noreply.github.com>
2020-04-20 17:14:31 -0700

    Fixing Images
    Attempt to fix another image. Created midterm dry run copy and remaned to midterm.

---
+++ Homework/Midterm/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $('#bugCarousel').carousel({
+        interval: 580
+    });
+
+    $('#carouselControl').click(function(){
+        
+        if( $('#carouselControl').hasClass('paused') ){
+            $('#bugCarousel').carousel('cycle');
+            $('#carouselControl').text('Pause');
+        }
+        else {
+            $('#myCarousel').carousel('pause');
+            $('#carouselControl').text('Play');
+        }
+
+        $('#carouselControl').toggleClass('paused');
+    });
+});
